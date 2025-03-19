d5b44a5e38dcc176dcaf4308c9146089f84a8e08
brenoas1 <brenooasantos@gmail.com>
2025-03-18 22:02:11 -0300

    Meu desafio de projeto DIO

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-let nome = "Breno"
+let nomeDoHeroi = "Breno"
+let experienciaDoHeroi = 1001
 
-console.log(nome)
+if (experienciaDoHeroi <= 1000) {
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Ferro")
+
+} else if(experienciaDoHeroi <= 2000){
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Bronze")
+
+}else if (experienciaDoHeroi <= 5000){
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Prata")
+
+}else if (experienciaDoHeroi <= 7000){
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Ouro")
+
+}else if (experienciaDoHeroi <= 8000){
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Platina")
+
+}else if (experienciaDoHeroi <= 9000){
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Ascendente")
+
+}else if (experienciaDoHeroi <= 10000){
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Imortal")
+
+}else if (experienciaDoHeroi > 10000){
+    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Radiante")
+}
+
