5093c9dc877754aa02c9265b624ada5558413eea
brenoas1 <brenooasantos@gmail.com>
2025-03-18 22:03:39 -0300

    Meu desafio de projeto DIO

--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@ let nomeDoHeroi = "Breno"
 let experienciaDoHeroi = 1001
 
 if (experienciaDoHeroi <= 1000) {
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Ferro")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Ferro")
 
 } else if(experienciaDoHeroi <= 2000){
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Bronze")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Bronze")
 
 }else if (experienciaDoHeroi <= 5000){
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Prata")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Prata")
 
 }else if (experienciaDoHeroi <= 7000){
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Ouro")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Ouro")
 
 }else if (experienciaDoHeroi <= 8000){
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Platina")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Platina")
 
 }else if (experienciaDoHeroi <= 9000){
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Ascendente")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Ascendente")
 
 }else if (experienciaDoHeroi <= 10000){
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Imortal")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Imortal")
 
 }else if (experienciaDoHeroi > 10000){
-    console.log("O Herói de nome: " + nomeDoHeroi + " está no nível Radiante")
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível Radiante")
 }
 
